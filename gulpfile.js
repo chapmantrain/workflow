426a8569cec6fd65d1274a99209973a315eb63c3
Norm Bedwell <Norm Bedwell>
2016-12-20 16:48:10 -0500

    Added compass stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,32 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 var jsSources = [
   'components/scripts/test01.js',
   'components/scripts/test02.js'
 ];
-gulp.task('log', function() {
+
+var sassSources = [
+  'components/sass/style.scss'
+];
+
   gutil.log("Hello World!");
-});
+
 
 gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
 });
